f5bb90504e37bd95995de8d685585a4a1b1f7c1a
Jhoel Ivan Macias Mamani <43515294-jhoelmacias30@users.noreply.replit.com>
2025-06-10 13:37:16 +0000

    ejercicio en clases

---
+++ tabla.py
@@ -0,0 +1,5 @@
+num_tabla = int(input("ingrese un numero para ver tapla de multiplicar:"))
+print(f"\n--- Tabla del{num_tabla}---")
+for i in range (1, 11):
+    resultado = num_tabla * i
+    print(f"{num_tabla} x {i} = {resultado} ")
