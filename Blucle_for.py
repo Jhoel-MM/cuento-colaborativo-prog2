11414beb075c761fee1f5e4d7d623d193eb95c0a
Jhoel Ivan Macias Mamani <43515294-jhoelmacias30@users.noreply.replit.com>
2025-06-10 12:38:24 +0000

    Ejercicio condicionales: verificador de edad para peliculas

---
+++ Blucle_for.py
@@ -0,0 +1,6 @@
+for numero in range(3):
+  print(numero)
+print("\nRecoriendo un string:")
+nombre= "PYTHON"
+for letra in nombre:
+  print(letra)
---
+++ VERIFICACION_DE_EDAD.py
@@ -0,0 +1,10 @@
+edad = 20
+
+if edad >= 18:
+    print("es Mayor de edad. ¡puede votar y condicir (con licencia)!")
+elif edad >= 13:
+    print("Es un adolecente")
+elif edad < 0:
+    print("edad invalidad. ¡Los viajes en el tiempo aun no son comereciales!")
+else:
+    print ("es un niño o niña.")
