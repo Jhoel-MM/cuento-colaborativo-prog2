2ee3219a199ddddfedd05fb2bd0360cff8f51f78
Jhoel Ivan Macias Mamani <43515294-jhoelmacias30@users.noreply.replit.com>
2025-06-11 13:25:38 +0000

    promedio de clase

---
+++ mis_notas.py
@@ -0,0 +1,11 @@
+mis_notas = [85.5, 92, 78, 88.5, 95, 82]
+suma_total = 0
+
+for nota in mis_notas:
+    suma_total += nota
+
+promedio = suma_total / len(mis_notas)
+
+print(f"suma total de las notas: {suma_total}")
+print(f"promedio de las notas: {promedio:.2f}")
+print("Fin del programa Jhoel Ivan Macias Mamani")
